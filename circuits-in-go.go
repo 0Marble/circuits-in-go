698c1fd892013ed8eae83ee774c153b2d05f96d6
Aleksei Lobanov <zeromarblectm@gmail.com>
2023-07-28 19:59:00 +0800

    setting up modules

--- circuits-in-go.go
+++ circuits-in-go.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"github.com/0Marble/circuits-in-go/utils"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	utils.MyFunc()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/0Marble/circuits-in-go
 
 go 1.20
+
+require github.com/0Marble/circuits-in-go/utils v0.0.0-20230728114640-ccaac9067233 // indirect
